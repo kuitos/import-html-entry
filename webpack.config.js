45a6098c6bfbac1cb0c7d6413c7943dcc642ca6f
Kuitos <kuitos.lau@gmail.com>
2018-09-07 15:13:48 +0800

    :wrench: distinguish normal and minimization

--- webpack.config.js
+++ webpack.config.js
@@ -6,31 +6,43 @@
 
 const path = require('path');
 
-module.exports = {
-	entry: {
-		'import-html': './src/index.js'
-	},
-	devtool: 'source-map',
-	output: {
-		path: path.resolve(__dirname, './lib'),
-		filename: '[name].js',
-		libraryTarget: 'umd',
-		library: 'importHTML',
-		libraryExport: 'default'
-	},
-	mode: 'production',
-	resolve: {
-		alias: {
-			systemjs: path.resolve(__dirname, './node_modules/systemjs/dist/system-production.src.js')
-		}
-	},
-	module: {
-		rules: [
-			{
-				test: '/\.js$/',
-				use: 'babel-loader',
-				exclude: '/node_modules/'
+module.exports = env => {
+
+	const minimize = !!(env && env.minimize);
+	const appName = minimize ? 'import-html-entry.min' : 'import-html-entry';
+
+	return {
+		entry: {
+			[appName]: './src/index.js'
+		},
+		devtool: 'source-map',
+		output: {
+			path: path.resolve(__dirname, './lib'),
+			filename: '[name].js',
+			libraryTarget: 'umd',
+			library: 'importHTML',
+			libraryExport: 'default'
+		},
+		mode: 'production',
+		optimization: {
+			minimize
+		},
+		node: {
+			process: false
+		},
+		resolve: {
+			alias: {
+				systemjs: path.resolve(__dirname, './node_modules/systemjs/dist/system-production.src.js')
 			}
-		]
-	}
+		},
+		module: {
+			rules: [
+				{
+					test: '/\.js$/',
+					use: 'babel-loader',
+					exclude: '/node_modules/'
+				}
+			]
+		}
+	};
 };
