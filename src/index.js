746ecf9154b56e1e5fd8ef4dad7ba105c8146279
Kuitos <kuitos.lau@gmail.com>
2018-12-27 02:44:06 +0800

    :sparkles: add more format support

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@
  */
 
 import loadjs from 'loadjs';
+import 'systemjs/dist/extras/amd';
+import 'systemjs/dist/extras/named-exports';
 import 'systemjs/dist/s';
-import 'systemjs/dist/extras/global';
 import processTpl from './process-tpl';
 
 function getDomain(url) {
