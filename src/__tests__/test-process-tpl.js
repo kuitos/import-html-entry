4e62fa17e75b705e74c9d89a68e46e6659281da1
Kuitos <kuitos.lau@gmail.com>
2018-12-26 20:49:01 +0800

    :sparkles: add stylesheet processing

---
+++ src/__tests__/test-process-tpl.js
@@ -0,0 +1,42 @@
+import processTpl from '../process-tpl';
+
+test('test process-tpl', () => {
+
+	const tpl = '<!DOCTYPE html><html><head>\n' +
+		'\n' +
+		'<link rel="shortcut icon" href="https://t.alipayobjects.com/images/rmsweb/T1pqpiXfJgXXXXXXXX.png" type="image/x-icon">\n' +
+		'<link rel="stylesheet" href="/umi.css">\n' +
+		'\n' +
+		'<meta charset="utf-8">\n' +
+		'<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">\n' +
+		'<title>&#x91D1;&#x878D;&#x4E91;&#x63A7;&#x5236;&#x53F0;</title>\n' +
+		'<script>\n' +
+		'  window.routerBase = "/";\n' +
+		'</script>\n' +
+		'<script>\n' +
+		'  \n' +
+		'        // bigfish version: 2.7.2\n' +
+		'        // umi version: 2.2.8\n' +
+		'        // build time: Wed Dec 26 2018 17:54:47 GMT+0800 (CST)\n' +
+		'      \n' +
+		'</script>\n' +
+		'<script src="//gw.alipayobjects.com/as/g/antcloud-fe/antd-cloud-nav/0.2.22/antd-cloud-nav.min.js"></script>\n' +
+		'</head>\n' +
+		'<body>\n' +
+		'\n' +
+		'<div id="root"></div>\n' +
+		'\n' +
+		'<script src="/umi.js"></script>\n' +
+		'\n' +
+		'\n' +
+		'</body></html>';
+
+	const { entry, scripts, template } = processTpl(tpl, 'http://kuitos.me');
+	expect(entry).toBe('http://kuitos.me/umi.js');
+	expect(scripts).toEqual([
+		'//gw.alipayobjects.com/as/g/antcloud-fe/antd-cloud-nav/0.2.22/antd-cloud-nav.min.js',
+		'http://kuitos.me/umi.js',
+	]);
+	expect(template.indexOf('http://kuitos.me/umi.css') !== -1).toBeTruthy();
+
+});
--- src/process-tpl.js
+++ src/process-tpl.js
@@ -7,10 +7,18 @@
 const SCRIPT_TAG_REGEX = /<(script)\s+((?!type=('|')text\/ng-template\3).)*?>.*?<\/\1>/gi;
 const SCRIPT_SRC_REGEX = /.*\ssrc=('|")(\S+)\1.*/;
 const SCRIPT_ENTRY_REGEX = /.*\sentry\s*.*/;
+const LINK_TAG_REGEX = /<(link)\s+.*?>/gi;
+const STYLE_TYPE_REGEX = /\s+rel=("|')stylesheet\1.*/;
+const STYLE_HREF_REGEX = /.*\shref=('|")(\S+)\1.*/;
+
+function hasProtocol(url) {
+	return url.startsWith('//') || url.startsWith('http');
+}
 
 /**
  * parse the script link from the template
  * @param tpl
+ * @param domain
  * @returns {{template: void | string | *, scripts: *[], entry: *}}
  */
 export default function processTpl(tpl, domain) {
@@ -18,30 +26,51 @@ export default function processTpl(tpl, domain) {
 	let scripts = [];
 	let entry = null;
 
-	const template = tpl.replace(SCRIPT_TAG_REGEX, match => {
+	const template = tpl
 
-		const matchedScriptEntry = match.match(SCRIPT_ENTRY_REGEX);
-		const matchedScriptSrcMatch = match.match(SCRIPT_SRC_REGEX);
-		let matchedScriptSrc = matchedScriptSrcMatch && matchedScriptSrcMatch[2];
+		// change the css link
+		.replace(LINK_TAG_REGEX, match => {
 
-		if (entry && matchedScriptEntry) {
-			throw new SyntaxError('You should not set multiply entry script!');
-		} else {
-
-			// append the domain while the script not have an protocol prefix
-			if(matchedScriptSrc && (!matchedScriptSrc.startsWith('//') && !matchedScriptSrc.startsWith('http'))) {
-				matchedScriptSrc = domain + matchedScriptSrc;
+			const styleType = !!match.match(STYLE_TYPE_REGEX);
+			if (styleType) {
+				const styleHref = match.match(STYLE_HREF_REGEX);
+				if (styleHref) {
+					const href = styleHref && styleHref[2];
+					if (href && !hasProtocol(href)) {
+						const newHref = domain + href;
+						return match.replace(href, newHref);
+					}
+				}
 			}
 
-			entry = entry || matchedScriptEntry && matchedScriptSrc;
-		}
+			return match;
+		})
 
-		if (matchedScriptSrc) {
-			scripts.push(matchedScriptSrc);
-		}
+		.replace(SCRIPT_TAG_REGEX, match => {
 
-		return `<!-- script ${matchedScriptSrc} replaced -->`;
-	});
+			const matchedScriptEntry = match.match(SCRIPT_ENTRY_REGEX);
+			const matchedScriptSrcMatch = match.match(SCRIPT_SRC_REGEX);
+			let matchedScriptSrc = matchedScriptSrcMatch && matchedScriptSrcMatch[2];
+
+			if (entry && matchedScriptEntry) {
+				throw new SyntaxError('You should not set multiply entry script!');
+			} else {
+
+				// append the domain while the script not have an protocol prefix
+				if (matchedScriptSrc && !hasProtocol(matchedScriptSrc)) {
+					matchedScriptSrc = domain + matchedScriptSrc;
+				}
+
+				entry = entry || matchedScriptEntry && matchedScriptSrc;
+			}
+
+			if (matchedScriptSrc) {
+				scripts.push(matchedScriptSrc);
+				return `<!-- script ${matchedScriptSrc} replaced -->`;
+			}
+
+			return match;
+		});
 
 	scripts = scripts.filter(function (script) {
 		// filter empty script
