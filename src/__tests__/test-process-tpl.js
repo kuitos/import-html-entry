28aed09d7dbc5d5c7085dd69793e1801a578e0bf
howel52 <401738000@qq.com>
2019-10-14 10:48:00 +0800

    :white_check_mark: new case mixing quotation marks

--- src/__tests__/test-process-tpl.js
+++ src/__tests__/test-process-tpl.js
@@ -184,3 +184,72 @@ test('test resource with no quotation marks', () => {
 	expect(template2.indexOf(genLinkReplaceSymbol('http://kuitos.me/cdn/umi.css')) !== -1).toBeTruthy();
 
 });
+
+test('test resource mixing quotation marks', () => {
+
+	const tpl = '<!DOCTYPE html><html><head>\n' +
+		'\n' +
+		'<link rel="shortcut icon" href=https://t.alipayobjects.com/images/rmsweb/T1pqpiXfJgXXXXXXXX.png type="image/x-icon">\n' +
+		'<link rel=stylesheet href="/umi.css">\n' +
+		'\n' +
+		'<meta charset="utf-8">\n' +
+		'<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">\n' +
+		'<title>&#x91D1;&#x878D;&#x4E91;&#x63A7;&#x5236;&#x53F0;</title>\n' +
+		'<script data-test>\n' +
+		'  window.routerBase = "/";\n' +
+		'</script>\n' +
+		'<script \n ' +
+		'data-test>\n' +
+		'  window.routerBase = "/";\n' +
+		'</script>\n' +
+		'<script>\n' +
+		'\n' +
+		'// umi version: 2.2.8\n' +
+		'// build time: Wed Dec 26 2018 17:54:47 GMT+0800 (CST)\n' +
+		'\n' +
+		'</script>\n' +
+		'<script src="//gw.alipayobjects.com/as/g/antcloud-fe/antd-cloud-nav/0.2.22/antd-cloud-nav.min.js"></script>\n' +
+		'<script \n' +
+		'  src=https://gw.alipayobjects.com/os/lib/react/16.8.6/umd/react.production.min.js\n' +
+		'  crossorigin="anonymous"' +
+		'></script>' +
+		'<style>\n' +
+		'body {\n' +
+		'background-color: red;\n' +
+		'}\n' +
+		'</style>\n' +
+		'</head>\n' +
+		'<body>\n' +
+		'\n' +
+		'<div id="root"></div>\n' +
+		'\n' +
+		'<script src=/umi.js></script>\n' +
+		'<!-- <script src=/a1.js></script>' +
+		'-->' +
+		'<script src="/comment.js"></script>\n' +
+		'<!-- <script src=/a2.js></script>\n' +
+		'-->' +
+		'<!--[if IE 6]>\n' +
+		'<!-- <script src=/a3-ie6-polyfill.js></script>\n' +
+		'<![endif]-->' +
+		'\n' +
+		'\n' +
+		'</body></html>';
+
+	const { entry, scripts, template } = processTpl(tpl, 'http://kuitos.me');
+	expect(entry).toBe('http://kuitos.me/comment.js');
+	expect(scripts).toEqual(['<script data-test>\n  window.routerBase = "/";\n</script>',
+		'<script \n data-test>\n  window.routerBase = "/";\n</script>',
+		'//gw.alipayobjects.com/as/g/antcloud-fe/antd-cloud-nav/0.2.22/antd-cloud-nav.min.js',
+		'https://gw.alipayobjects.com/os/lib/react/16.8.6/umd/react.production.min.js',
+		'http://kuitos.me/umi.js',
+		'http://kuitos.me/comment.js']);
+	expect(template.indexOf(genLinkReplaceSymbol('http://kuitos.me/umi.css')) !== -1).toBeTruthy();
+	expect(template.indexOf(genScriptReplaceSymbol('http://kuitos.me/umi.js')) !== -1).toBeTruthy();
+	expect(template.indexOf(genScriptReplaceSymbol('http://kuitos.me/comment.js')) !== -1).toBeTruthy();
+
+	const { styles, template: template2 } = processTpl(tpl, 'http://kuitos.me/cdn');
+	expect(styles[0]).toBe('http://kuitos.me/cdn/umi.css');
+	expect(template2.indexOf(genLinkReplaceSymbol('http://kuitos.me/cdn/umi.css')) !== -1).toBeTruthy();
+
+});
