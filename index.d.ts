d90c1d09452fb5d1120b62531a517171c6922262
Kuitos <kuitos.lau@gmail.com>
2019-11-19 11:55:19 +0800

    :label: add execScripts typing

--- index.d.ts
+++ index.d.ts
@@ -21,6 +21,8 @@ type ImportEntryOpts = {
 
 export type Entry = string | { styles?: string[], scripts?: string[], html?: string };
 
+export function execScripts<T>(entry: string | null, scripts: string[], proxy: Window, opts?: ImportEntryOpts): Promise<T>;
+
 export default function importHTML(url: string, fetch?: Function): Promise<IImportResult>;
 
 export function importEntry(entry: Entry, opts?: ImportEntryOpts): Promise<IImportResult>;
